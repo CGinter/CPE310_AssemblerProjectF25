28f099b9481756281f368a6e4a42dbe4bd036017
Chase Sprigle <chase.sprigle@protonmail.com>
2025-10-22 12:59:13 -0400

    Major refactor
    Created constant UNDEFINED for use with convert_to_machine_code() and
    other functions returning uint32_t where the value of return may be
    undefined. Moved all struct and enum definitions to types.h. Moved all
    constants to consts.c/h. Created util.c/h for utility functions and
    moved bin_to_num() there. Created assm_to_mach.c/h and moved
    convert_to_machine_code() there. Broke convert_to_machine_code() into
    smaller pieces as it was rather large. Lastly, created include.h for all
    includes to simplify development.

---
+++ assm_to_mach.h
@@ -0,0 +1,1 @@
+uint32_t convert_to_machine_code(struct assm_parse_result parsed, char **error);
---
+++ types.h
@@ -0,0 +1,36 @@
+enum assm_parse_result_type {
+	REGISTER,
+	IMMEDIATE,
+	TARGET,
+	NONE
+};
+
+struct assm_parse_result {
+	char *op_name;
+	enum assm_parse_result_type types[4];
+	uint32_t vals[4];
+};
+
+enum instruction_type {
+	R_TYPE,
+	I_TYPE,
+	J_TYPE
+};
+
+enum instruction_part {
+	RD,
+	RS,
+	RT,
+	SA,
+	IMM,
+	TAR,
+	EMPTY
+};
+
+struct instruction_definition {
+	char *op_name;
+	enum instruction_type type;
+	char *op_code;
+	char *funct_code;
+	enum instruction_part parts[4];
+};
