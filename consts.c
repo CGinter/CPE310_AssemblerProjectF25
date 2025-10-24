feff2673fad8c9f9da38eed541c761d7b905851d
vmarston13 <vmarston32@gmail.com>
2025-10-23 20:25:09 -0400

    These are 19 of the R_Type files

--- consts.c
+++ consts.c
@@ -1,5 +1,11 @@
+/**
+ * @file consts.c
+ * @authors Chase Sprigle and Victoria Marston
+ * @date 10/23/25
+ */
 #include "include.h"
 
+//Chase Sprigle Start
 const uint32_t UNDEFINED;
 
 const uint32_t MAX_5_BIT = 31;
@@ -21,5 +27,122 @@ const struct instruction_definition instruction_definitions[] = {
 		"000000",
 		"011010",
 		{ RS, RT, EMPTY, EMPTY }
+	}
+	//Chase Sprigle End
+	//Victoria Marston Start
+	{
+		"ADDU",
+		R_Type,
+		"000000",
+		"100001",
+		{ RD, RS, RT, EMPTY }
+	},
+	{
+		"AND",
+		R_Type,
+		"000000",
+		"100100",
+		{ RD, RS, RT, EMPTY }
+	},
+	{
+		"DIVU",
+		R_Type,
+		"000000",
+		"011011",
+		{ EMPTY, RS, RT, EMPTY }
+	},
+	{
+		"JALR",
+		R_Type,
+		"000000",
+		"001001",
+		{ RD, RS, EMPTY, EMPTY }
+	},
+	{
+		"JR",
+		R_Type,
+		"000000",
+		"001000",
+		{ EMPTY, RS, EMPTY, EMPTY }
+	},
+	{
+		"MFHI",
+		R_Type,
+		"000000".
+		"010000",
+		{ RD, EMPTY, EMPTY, EMPTY }
+	},
+	{
+		"MFLO",
+		R_Type,
+		"000000",
+		"010010",
+		{RD, EMPTY, EMPTY, EMPTY }
+	},
+	{
+		"MTHI",
+		R_Type,
+		"000000",
+		"010001",
+		{ EMPTY, RS, EMPTY, EMPTY }
+	},
+	{
+		"MTLO",
+		R_Type,
+		"000000",
+		"010011",
+		{ EMPTY, RS, EMPTY, EMPTY }
+	},
+	{
+		"MULT",
+		R_Type,
+		"000000",
+		"011000",
+		{ EMPTY, RS, RT, EMPTY}
+	},
+	{
+		"MULTU",
+		R_Type,
+		"000000",
+		"011001",
+		{ EMPTY, RS, RT, EMPTY }
+	},
+	{
+		"NOR",
+		R_Type,
+		"000000",
+		"100111",
+		{ RD, RS, RT, EMPTY }
+	},
+	{
+		"OR",
+		R_Type,
+		"000000",
+		"100101",
+		{ RD, RS, RT, EMPTY }
+	},
+	{
+		"SLL",
+		R_Type,
+		"000000",
+		"000000",
+		{ RD, EMPTY, RT, SA }
+	},
+	{
+		"SLLV",
+		R_Type,
+		"000000",
+		"000100",
+		{ RD, EMPTY, RT, RS }
+	},
+	{
+		"SLT",
+		R_Type,
+		"000000",
+		"101010",
+		{ RD, RS, RT, EMPTY }
+	},
+	{
+		
 	}
 };
--- include.h
+++ include.h
@@ -2,7 +2,7 @@
 #define INCLUDE_H
 
 #include <stdbool.h>
-#include <stdint.h>
+#include <stdint.h>  
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
