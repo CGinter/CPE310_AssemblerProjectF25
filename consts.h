81a9fde19725dcdd3af7af0e7f9814d7ccd7af84
vmarston13 <vmarston32@gmail.com>
2025-10-23 22:05:48 -0400

    All of table values and the updates to consts.h

--- consts.h
+++ consts.h
@@ -5,4 +5,4 @@ extern const uint32_t MAX_6_BIT;
 extern const uint32_t MAX_16_BIT;
 extern const uint32_t MAX_26_BIT;
 
-extern const struct instruction_definition instruction_definitions[2];
+extern const struct instruction_definition instruction_definitions[53];
