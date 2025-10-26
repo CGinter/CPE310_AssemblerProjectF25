1efe491ffa0755a07bef08b26da75df2a3c326ec
CGinter <caden.ginter@comcast.net>
2025-10-26 16:09:54 -0400

    All user input-related stuff implemented. Still needs attached to processing code

--- main.c
+++ main.c
@@ -31,7 +31,40 @@ int main(int argc, char *argv[])
 	while (1) {
 		char line[LINE_BUFF_SIZE];
 		get_next_input(line, &int_state, flags, in_filename, &in_file);
-		printf("%s\n", line);
+		// printf("%s\n", line);
+		switch (int_state) {
+			case ASM_TO_MACH:
+				// Call conversion from assembly to binary, placeholder value for now
+				// uint32_t result = 0b10101010101010101100110011110000;
+				uint32_t result = 0b00000000000000000000000000001000;
+				// Print the result in binary and hexadecimal
+				printf("Hex: 0x%08X Binary:", result);
+				for (int i = 0; i < 32; i++) {
+					if (i % 4 == 0 && i != 0)
+						printf(" ");
+					printf("%d", result / 0b010000000000000000000000000000000);
+					result *= 2;
+				}
+				printf("\n");
+				break;
+			case HEX_TO_ASM:
+				// First convert string hexadecimal to string binary
+				char* end;
+				uint32_t hex_in = strtol(line, &end, 16);
+				char bin_in[33];
+				for (int i = 0; i < 32; i++) {
+					bin_in[i] = (hex_in / 0b010000000000000000000000000000000) ? '1' : '0';
+					hex_in *= 2;
+				}
+				bin_in[32] = '\0';
+				// Call binary to assembly conversion and print out, print input in binary for now
+				printf("%s\n\n", bin_in);
+				break;
+			case BIN_TO_ASM:
+				// Call binary to assembly conversion and print result, print input in binary for now
+				printf("%s\n\n", line);
+				break;
+		}
 	}
 	// if (result & ARG_AUTO)
 	// 	printf("Auto mode\n");
